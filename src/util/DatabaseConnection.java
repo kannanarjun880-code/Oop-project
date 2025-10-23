8dd316d028da864d14321e944752188e5f766363
kannanarjun880-code <kannanarjun880@gmail.com>
2025-10-23 13:57:23 +0530

    Create DatabaseConnection.java

---
+++ src/util/DatabaseConnection.java
@@ -0,0 +1,38 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/online_quiz_system";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+    
+    private static Connection connection;
+    
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL JDBC Driver not found", e);
+        }
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        return connection;
+    }
+    
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
